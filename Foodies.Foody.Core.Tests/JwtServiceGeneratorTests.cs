d8065df7999663c51b9597a44ad0c2cea89b5f66
Gino Messmer <ginomessmer@outlook.com>
2019-11-29 09:04:36 +0000

    Merged PR 8: Implement JWT Service
    - Add generator test for JWT service
    - Finalize JWT token service and tests
    
    Related work items: #142

---
+++ Foodies.Foody.Core.Tests/JwtServiceGeneratorTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Unicode;
+using System.Transactions;
+using Foodies.Foody.Core.Security;
+using Microsoft.IdentityModel.Tokens;
+using Xunit;
+
+namespace Foodies.Foody.Core.Tests
+{
+    public class JwtServiceGeneratorTests
+    {
+        [Fact]
+        public void Create_NewToken_CanGenerate()
+        {
+            // Arrange
+            var service = CreateDefaultTokenService();
+            var userId = "12345";
+
+            // Act
+            string jwt = service.WriteToken(new Dictionary<string, string> {{ "uid", userId }}, 
+                meta: new JwtTokenMeta {
+                    ExpiresAt = DateTime.Now.AddDays(1),
+                    Audience = "foody",
+                    Issuer = "foody"
+                });
+
+            // Assert
+            Assert.NotEmpty(jwt);
+        }
+
+        [Fact]
+        public void Validate_Token_Successfully()
+        {
+            // Arrange
+            var services = CreateDefaultTokenService();
+            var userId = "12345";
+            var audience = "foody";
+            var issuer = "foody";
+
+            // Act
+            string jwt = services.WriteToken(new Dictionary<string, string> {{"uid", userId}},
+                new JwtTokenMeta
+                {
+                    ExpiresAt = DateTime.Now.AddDays(1),
+                    Audience = audience,
+                    Issuer = issuer
+                });
+
+            // Assert
+            var isValid = services.ValidateToken(jwt, new TokenValidationParameters 
+            {
+                ValidAudience = audience,
+                ValidIssuer = issuer
+            });
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void Validate_TokenWithWrongParameters_Fail()
+        {
+            // Arrange
+            var services = CreateDefaultTokenService();
+            const string userId = "12345";
+            const string audience = "foody";
+            const string issuer = "foody";
+            const string validateAudience = "dhbw";
+            const string validateIssuer = issuer;
+
+            // Act
+            var jwt = services.WriteToken(new Dictionary<string, string> { { "uid", userId } },
+                new JwtTokenMeta
+                {
+                    ExpiresAt = DateTime.Now.AddDays(1),
+                    Audience = audience,
+                    Issuer = issuer
+                });
+
+            // Assert
+            var isValid = services.ValidateToken(jwt, new TokenValidationParameters
+            {
+                ValidAudience = validateAudience, ValidIssuer = validateIssuer
+            });
+
+            Assert.False(isValid);
+        }
+
+        [Fact]
+        public void ReadNameClaim_Token_Successfully()
+        {
+            // Arrange
+            var services = CreateDefaultTokenService();
+            const string userId = "12345";
+            const string audience = "foody";
+            const string issuer = "foody";
+
+            // Act
+            var jwt = services.WriteToken(new Dictionary<string, string> {{ ClaimTypes.Name, userId }},
+                new JwtTokenMeta
+                {
+                    ExpiresAt = DateTime.Now.AddDays(1),
+                    Audience = audience,
+                    Issuer = issuer
+                });
+
+            var claims = services.ReadToken(jwt, new TokenValidationParameters
+            {
+                ValidAudience = audience, ValidIssuer = issuer
+            });
+
+            // Assert
+            Assert.Equal(userId, claims.Identity.Name);
+        }
+
+        // Seed work
+        private JwtTokenService CreateDefaultTokenService()
+        {
+            var secret = "verysecuresecretindeed!123";
+            return new JwtTokenService(signingKey: new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)));
+        }
+    }
+}
---
+++ Foodies.Foody.Core/Security/JwtTokenMeta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Foodies.Foody.Core.Security
+{
+    public class JwtTokenMeta
+    {
+        public DateTime ExpiresAt { get; set; }
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+    }
+}
